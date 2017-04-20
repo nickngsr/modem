9ff9c27e46e7d729ae9015349ba025a683486a6e
spaceguy <shreyasponkshe12345@gmail.com>
2017-04-20 20:39:15 +0530

    This commit might solve the timeout issue on modem not responding

--- examples/at.js
+++ examples/at.js
@@ -1,30 +1,58 @@
 var pdu = require('pdu');
 var modem = require('../index.js').Modem();
 modem.open('/dev/ttyUSB1', function() {
-	var encoded = "*121#";
-
-	modem.execute('ATZ +CFUN=1', function(escape_char, response){
+var encoded = "*121#";
+/*
+	modem.execute('AT+CPIN?', function(escape_char, response){
 		//console.log(escape_char + response);
-			setTimeout(function(){
-				//modem.execute('AT+CSCS="GSM"', function(escape_char, response){
-					//console.log(escape_char + response);
-						//modem.execute('AT+CUSD=1', function(escape_char, response){
-
+		setTimeout(function(){
+			modem.execute('AT+CPIN="1234"', function(escape_char, response){
+				//console.log(escape_char + response);
+					setTimeout(function(){
+						modem.execute('AT+CPIN?', function(escape_char, response){
 							//console.log(escape_char + response);
-							modem.execute('AT+CUSD=1,"*121*44#",15', function(escape_char, response){
-								
-								console.log(escape_char + response);
+							setTimeout(function(){
+								modem.execute('ATZ +CFUN=1', function(escape_char, response){
 									setTimeout(function(){
-										modem.execute('AT+CUSD=2', function(escape_char, response){
-										
+									//console.log(escape_char + response);
+										modem.execute('AT+CUSD=1,"*121*44#",15', function(escape_char, response){
+											
 											console.log(escape_char + response);
+												setTimeout(function(){
+													modem.execute('AT+CUSD=2', function(escape_char, response){
+														//console.log(escape_char + response);
+													});
+												},3000);
+
 										});
-									},10000);
+									},9000);
+								});
+							},9000);
+						});
+					},9000);
 
-							});
-						//});
-				//});
-			},10000);
-
+			});
+		},9000);
 	});
+*/
+
+
+								modem.execute('ATZ +CFUN=1', function(escape_char, response){
+									setTimeout(function(){
+									//console.log(escape_char + response);
+										modem.execute('AT+CUSD=1,"*121*44#",15', function(escape_char, response){
+											
+											console.log(escape_char + response);
+												setTimeout(function(){
+													modem.execute('AT+CUSD=2', function(escape_char, response){
+														//console.log(escape_char + response);
+													});
+												},3000);
+
+										});
+									},9000);
+								});
+
+
+
 });
